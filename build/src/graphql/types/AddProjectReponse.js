00aad5f416571d9e48eff38823c9944bc1409761
brdlysmth <brsmith110@gmail.com>
2020-10-03 21:36:42 -0400

    removing unnecessary files for github

--- build/src/graphql/types/AddProjectReponse.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AddProjectResponse = void 0;
-exports.AddProjectResponse = {
-    name: function (e) { return e.name; },
-};
